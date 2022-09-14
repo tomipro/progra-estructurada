d7e0be32319e96412e7cbe808005c95050995874
tomipro <tomy.prodan@gmail.com>
2022-09-14 13:41:16 -0300

    ej 3

--- practica-02/3/3.c
+++ practica-02/3/3.c
@@ -1,71 +1,105 @@
-// wip
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-#define TAM 50
-#define F 50
-#define C 50
-#define TERM_DATO -999
-#define TERM_STR '\0'
+#define T 50
+#define N 10
 
 struct s_datos
 {
-    char nomEquipo[TAM];
-    int pg, pp, pe, gf, gc;
+    char nomEquipo[T];
+    int pts, pj, pg, pe, pp, gf, gc, dif;
 };
 typedef struct s_datos t_datos;
 
-void cargarDatos(t_datos datos[TAM], char archItems[TAM], char archDatos[TAM]);
-void imprimirMat(t_datos datos);
+void cargarDatos(t_datos datos[N], char nomArchDatos[T], char nomArchItems[T]);
+void imprimirTabla(t_datos datos[N]);
+void ordenarTabla(t_datos datos[N], int col);
 
 int main(void)
 {
-    t_datos datos = {0};
+    t_datos datos[N] = {0};
 
-    cargarDatos(datos, "./items.txt", "./datos.txt");
-    imprimirMat(datos);
+    cargarDatos(datos, "./datos.txt", "./items.txt");
+
+    printf("Tabla Original:\n");
+    imprimirTabla(datos);
+
+    printf("\nTabla Ordenada Por Puntos:\n");
+    ordenarTabla(datos, 1);
+    imprimirTabla(datos);
+
+    printf("\nTabla Ordenada Por DIF:\n");
+    ordenarTabla(datos, 8);
+    imprimirTabla(datos);
+
+    printf("\nTabla Ordenada Por GF:\n");
+    ordenarTabla(datos, 6);
+    imprimirTabla(datos);
 
     return 0;
 }
 
-void cargarDatos(t_datos datos[TAM], char archItems[TAM], char archDatos[TAM])
+void cargarDatos(t_datos datos[N], char nomArchDatos[T], char nomArchItems[T])
 {
-    int i = 0, j = 0, r, leido = 0;
-    FILE *archI, *archD;
-    char aux;
+    int i = 0, j = 0, r;
+    FILE *archD, *archI;
 
-    archI = fopen(archItems, "r");
-    archD = fopen(archDatos, "r");
+    archD = fopen(nomArchDatos, "r");
+    archI = fopen(nomArchItems, "r");
 
-    while (i < F - 1 && !feof(archI) && !feof(archD))
+    r = fscanf(archD, "%d, %d, %d, %d, %d, %d, %d, %d\n", &datos[i].pts, &datos[i].pj, &datos[i].pg, &datos[i].pe, &datos[i].pp, &datos[i].gf, &datos[i].gc, &datos[i].dif);
+    for (i = 1; i < N; i++)
     {
-        t_datos dato;
-        fscanf(archD, "%d, %d, %d, %d, %d\n", &dato.pg, &dato.pp, &dato.pe, &dato.gf, &dato.gc);
-        
-        j = 0;
-        while (j < C - 1 && aux != '\n')
-        {
-            aux = fgetc(archI);
-            dato.nomEquipo[j] = aux;
-            j++;
-        }
-        dato.nomEquipo[j - 1] = TERM_STR;
-        i++;
+        r = fscanf(archD, "%d, %d, %d, %d, %d, %d, %d, %d\n", &datos[i].pts, &datos[i].pj, &datos[i].pg, &datos[i].pe, &datos[i].pp, &datos[i].gf, &datos[i].gc, &datos[i].dif);
     }
 
-    fclose(archI);
+    r = fscanf(archI, "%[^\n]\n", datos[j].nomEquipo);
+    for (j = 1; j < N; j++)
+    {
+        r = fscanf(archI, "%[^\n]\n", datos[j].nomEquipo);
+    }
+
     fclose(archD);
+    fclose(archI);
 }
 
-void imprimirMat(t_datos datos)
+void imprimirTabla(t_datos datos[N])
 {
-    int i = 0;
+    int i, j;
 
-    while (i < F - 1)
+    printf("%-20s %-3s %-3s %-3s %-3s %-3s %-3s %-3s %-3s\n", "Equipo", "PTS", "PJ", "PG", "PE", "PP", "GF", "GC", "DIF");
+    printf("-----------------------------------------------------\n");
+
+    for (i = 0; i < N && datos[i].pj != 0; i++)
     {
-        t_datos info = datos[i];
-        printf("%-15s %5d %5d %5d %5d %d\n", info.nomEquipo, info.pg, info.pp, info.pe, info.gf, info.gc);
-        i++;
+        printf("%-20s %-3d %-3d %-3d %-3d %-3d %-3d %-3d", datos[i].nomEquipo, datos[i].pts, datos[i].pj, datos[i].pg, datos[i].pe, datos[i].pp, datos[i].gf, datos[i].gc);
+        if (datos[i].dif <= 0)
+        {
+            printf(" %-3d\n", datos[i].dif);
+        }
+
+        else
+        {
+            printf(" +%-3d\n", datos[i].dif);
+        }
+    }
+}
+
+void ordenarTabla(t_datos datos[N], int col)
+{
+    int i, j;
+    t_datos aux;
+
+    for (i = 0; datos[i].pj != 0; i++)
+    {
+        for (j = 0; datos[j].pj != 0; j++)
+        {
+            if (((col == 1) && (datos[i].pts > datos[j].pts)) || (col == 6) && (datos[i].gf > datos[j].gf) || ((col == 8) && (datos[i].dif > datos[j].dif)))
+            {
+                aux = datos[i];
+                datos[i] = datos[j];
+                datos[j] = aux;
+            }
+        }
     }
 }
